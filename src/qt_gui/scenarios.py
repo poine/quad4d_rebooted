91cdd8e93ee6818fa65a2592a6524c8a2910f89a
Dieynaba <diopd@ept.sn>
2026-06-16 17:26:17 +0200

    Chanded numbers for drones and added some new scenarios for simulation

--- src/qt_gui/scenarios.py
+++ src/qt_gui/scenarios.py
@@ -9,23 +9,23 @@ class Scenario1:
     trajs= ["circle_with_intro1"]
 
 class Scenario2:
-    ids= [4, 5]
+    ids= [112, 113]
     trajs= ["circle_with_intro1", "circle_with_intro2"]
 
 class Scenario3:
-    ids= [4, 5, 6]
+    ids= [112, 113, 114]
     trajs= ["circle_with_intro1", "circle_with_intro2", "circle_with_intro3"]
 
 class Scenario4:
-    ids= [4, 5, 6, 7]
+    ids= [112, 113, 114, 115]
     trajs= ["circle_with_intro1", "circle_with_intro2", "circle_with_intro3", "circle_with_intro4"]
 
 class Scenario5:
-    ids= [4,5]
+    ids= [112, 113]
     trajs= ["smooth_back_and_forth1", "smooth_back_and_forth2"]
 
 class Scenario6:
-    ids = [4]
+    ids = [112]
     trajs = ["space indexed gate race1"]
     arena = "data/arena_3.yaml"
 
@@ -34,6 +34,22 @@ class Scenario7:
     trajs = ["scara race"]
     arena = "data/arena_4.yaml"
 
+class Scenario8:
+    ids= [112]
+    trajs= ["cercle_back_and_forth"]
 
-scenarios = [Scenario1, Scenario2, Scenario3, Scenario4, Scenario5, Scenario6, Scenario7]
+class Scenario9:
+    ids= [112, 113, 114]
+    trajs= ["smooth_back_and_forth1", "space indexed gate race1", "circle_with_intro1"]
+
+class Scenario10:
+    ids= [112, 113, 114]
+    trajs= ["space indexed oval", "space indexed figure of height2", "space indexed gate race1"]
+
+
+class Scenario11:
+    ids= [112, 113, 114]
+    trajs= ["queue leu leu 1", "queue leu leu 2", "queue leu leu 3"]
+
+scenarios = [Scenario1, Scenario2, Scenario3, Scenario4, Scenario5, Scenario6, Scenario7, Scenario8, Scenario9, Scenario10, Scenario11]
 
